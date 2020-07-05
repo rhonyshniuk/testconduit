c486792dde589b34aed179d351ee75770ed62261
aliiozo <alinaeichas@gmail.com>
2020-07-06 00:41:38 +0300

    TestLogs.txt

--- conduit/commands.py
+++ conduit/commands.py
@@ -13,8 +13,8 @@ HERE = os.path.abspath(os.path.dirname(__file__))
 PROJECT_ROOT = os.path.join(HERE, os.pardir)
 TEST_PATH = os.path.join(PROJECT_ROOT, 'tests')
 
-
-@click.command()
+cp
+@click.command()cpp
 def test():
     """Run the tests."""
     import pytest
